cf67080e5bb12dfbd9e9c46fae0a7a0734925436
skgc32 <191213129+skgc32@users.noreply.github.com>
2025-03-11 23:05:19 +0900

    修正　プロフィール画像

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:index, :show, :edit, :update]
-  resources :posts do
-    resources :comments, only:[:create, :destroy]
-    resource :favorites, only:[:create, :destroy]
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
   end
+    resources :posts do
+     resources :comments, only:[:create, :destroy]
+     resource :favorites, only:[:create, :destroy]
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
